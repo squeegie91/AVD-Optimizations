2b2642d40bd68c3689e3f91ba0d4e9e1d0c5fa91
squeegie91 <108372295+squeegie91@users.noreply.github.com>
2022-09-06 11:18:22 -0700

    Update OS-Optimization.ps1

--- OS-Optimization.ps1
+++ OS-Optimization.ps1
@@ -52,6 +52,7 @@ if (Test-Path $WinstationsKey) {
 
 Write-Host 'Settin up the Windows Firewall Rue for RDP ShortPath'
 New-NetFirewallRule -DisplayName 'Remote Desktop - Shortpath (UDP-In)' -Action Allow -Description 'Inbound rule for the Remote Desktop service to allow RDP traffic. [UDP 3390]' -Group '@FirewallAPI.dll,-28752' -Name 'RemoteDesktop-UserMode-In-Shortpath-UDP' -PolicyStore PersistentStore -Profile Domain, Private -Service TermService -Protocol udp -LocalPort 3390 -Program '%SystemRoot%\system32\svchost.exe' -Enabled:True
+Enable-NetFirewallRule -DisplayGroup 'Remote Desktop'
 
 ### Setting the Screen Protection
 
